591d4913e785bfaf0f6bdc13afb481476254e0c3
d0min0wang <38913318+d0min0wang@users.noreply.github.com>
2022-05-12 09:25:26 +0800

    Update 查询收缩数据库.sql

--- 查询收缩数据库.sql
+++ 查询收缩数据库.sql
@@ -1,5 +1,6 @@
 DBCC SQLPERF(LOGSPACE)
 
+select count(*) from icmo
 
 SELECT name 
 ,size/128.0 as size_MB
@@ -47,3 +48,25 @@ DBCC OPENTRAN
 
 SELECT session_id, elapsed_time_seconds
 FROM sys.dm_tran_active_snapshot_database_transactions
+
+--查看tempdb记录的分配情况
+use tempdb  
+go  
+SELECT top 10 t1.session_id,                                                      
+t1.internal_objects_alloc_page_count,  t1.user_objects_alloc_page_count,  
+t1.internal_objects_dealloc_page_count , t1.user_objects_dealloc_page_count,
+t3.login_name,t3.status,t3.total_elapsed_time  
+from sys.dm_db_session_space_usage  t1  
+inner join sys.dm_exec_sessions as t3  
+on t1.session_id = t3.session_id  
+where (t1.internal_objects_alloc_page_count>0  
+or t1.user_objects_alloc_page_count >0  
+or t1.internal_objects_dealloc_page_count>0  
+or t1.user_objects_dealloc_page_count>0)  
+order by t1.internal_objects_alloc_page_count desc
+
+--看这session_id的用处
+select p.*,s.text  
+from master.dbo.sysprocesses p  
+cross apply sys.dm_exec_sql_text(p.sql_handle) s  
+where spid = 202
