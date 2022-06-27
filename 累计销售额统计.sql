6d42366c4c986f5d0bf047fa2a2a119cc8b9fafb
d0min0wang <38913318+d0min0wang@users.noreply.github.com>
2022-06-27 16:45:49 +0800

    Update 累计销售额统计.sql

--- 累计销售额统计.sql
+++ 累计销售额统计.sql
@@ -55,28 +55,34 @@ FROM
 ;WITH Amount
 AS
 (
-SELECT format(v1.fdate, 'MM-dd') AS fdate, 
+SELECT cast(format(v1.fdate, 'MM-dd') AS nvarchar(5)) AS fdate, 
+    sum(case year(v1.FDate) when '2018' then u1.FConsignAmount else 0 end) AS FConsignAmount2018,
+    sum(case year(v1.FDate) when '2019' then u1.FConsignAmount else 0 end) AS FConsignAmount2019,
+    sum(case year(v1.FDate) when '2020' then u1.FConsignAmount else 0 end) AS FConsignAmount2020,
     sum(case year(v1.FDate) when '2021' then u1.FConsignAmount else 0 end) AS FConsignAmount2021,
     sum(case year(v1.FDate) when '2022' then u1.FConsignAmount else 0 end) AS FConsignAmount2022
     --FROM t_xySaleReporttest
     --select v1.FDate,v3.FName,v2.F_110,v2.Fname,u1.FAuxQty,u1.FConsignAmount
     FROM ICStockBill v1 
 	INNER JOIN ICStockBillEntry u1 ON u1.FInterID=v1.FInterID
-	where year(v1.FDate)IN ('2021','2022') 
+	--where year(v1.FDate)IN ('2020','2021','2022') 
 	--and month(v1.FDate)<='2'
 	
 	and v1.FTranType=21 
-    GROUP BY format(v1.fdate, 'MM-dd')
+    GROUP BY cast(format(v1.fdate, 'MM-dd') AS nvarchar(5))
 )
 SELECT
   fdate,
   --FConsignAmount2021,
-  FConsignAmount2022,
+  --FConsignAmount2022,
   --SUM(FConsignAmount2021) over () AS FAllAmount2021,
   --SUM(FConsignAmount2022) over () AS FAllAmount2022,
   --SUM(FConsignAmount2021) over (PARTITION BY fdate) AS FDateAmount2021,
   --SUM(FConsignAmount2022) over (PARTITION BY fdate) AS FDateAmount2022,
-  --SUM(FConsignAmount2021) over (ORDER BY fdate) AS FAccAmount2021,
+  SUM(FConsignAmount2018) over (ORDER BY fdate) AS FAccAmount2018,
+  SUM(FConsignAmount2019) over (ORDER BY fdate) AS FAccAmount2019,
+  SUM(FConsignAmount2020) over (ORDER BY fdate) AS FAccAmount2020,
+  SUM(FConsignAmount2021) over (ORDER BY fdate) AS FAccAmount2021,
   SUM(FConsignAmount2022) over (ORDER BY fdate) AS FAccAmount2022
 FROM
   Amount
