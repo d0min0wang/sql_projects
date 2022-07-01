8e48d570dc25f9fab6b4af63d43258d71f396ca3
d0min0wang <38913318+d0min0wang@users.noreply.github.com>
2022-07-01 11:50:24 +0800

    Update 每月分析-多事业部.sql

--- 每月分析-多事业部.sql
+++ 每月分析-多事业部.sql
@@ -22,7 +22,7 @@ use AIS20140921170539
 DECLARE @Period char(6)
 DECLARE @Department char(30)
 SET @Period='202206' --统计的年月
-SET @Department='电气连接国内事业部'
+SET @Department='新能源事业部'
 
 --SELECT MONTH(@Period+'01')
 
