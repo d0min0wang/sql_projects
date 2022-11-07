56b33e2724becbf89786321b2c24acc5074fc688
d0min0wang <38913318+d0min0wang@users.noreply.github.com>
2022-11-07 16:05:58 +0800

    新增

---
+++ 糊料成本导出.sql
@@ -0,0 +1,9 @@
+SELECT * FROM t_FIN_Raw_Material_Price
+--出库成本
+SELECT * FROM t_FIN_Ingredient_Cost
+--运费成本
+SELECT * FROM t_FIN_Shipping_Fee
+--利息成本
+SELECT * FROM t_FIN_Material_Inventory_Interest
+--总表
+SELECT * FROM t_FIN_Paste_Unit_Price WHERE FDate='2022-10'
