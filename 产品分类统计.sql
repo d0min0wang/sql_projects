d8405c9ebc7d226ed141edb4494859d9076cce83
d0min0wang <38913318+d0min0wang@users.noreply.github.com>
2023-07-14 16:50:39 +0800

    111

--- 产品分类统计.sql
+++ 产品分类统计.sql
@@ -14,7 +14,7 @@ Where (v1.FTranType = 21 AND (v1.FCancellation = 0)) and t3.FNumber like '93.%' 
 AND t4.FName LIKE '%将军%'
 Group by v1.FSupplyID
 
-SELECT * FROM t_Organization where fname like '%富士通%'
+SELECT * FROM t_Organization where 
 
 SELECT FHelpCode,* FROM t_ICItem where --fnumber like '93.%' or 
 FHelpCode LIKE 'GQ%'
@@ -75,3 +75,22 @@ order by FORMAT(v1.FDate, 'yyyy-MM') ASC
 select * FROM t_TableDescription where FTableName='ICStockBillEntry'
 
 SELECT * FROM t_FieldDescription where FTableID=210009 and FDescription LIKE '%单价%'
+
+--按客户
+Select Year(v1.FDate),
+    t4.fname, 
+    sum(case when t3.fnumber like '93.%' then u1.FConsignAmount else 0 END) AS 外购硅胶销售额,
+    sum(case when t3.fnumber like '93.%' then u1.FAuxQty else 0 END) AS 外购硅胶销售数量,
+    sum(case when t3.FHelpCode like 'GQ%' then u1.FConsignAmount else 0 end) AS 硅胶销售额, 
+     sum(case when t3.FHelpCode like 'GQ%' then u1.FAuxQty else 0 end) AS 硅胶销售数量, 
+    sum(u1.FConsignAmount) AS 销售额合计, 
+    sum(u1.FAuxQty) AS 销售数量合计
+From ICStockBill v1 
+inner join ICStockBillEntry u1 on u1.FInterID=v1.FInterID AND u1.FInterID <> 0
+left join t_ICItem t3 on u1.FItemID=t3.FItemID
+LEFT JOIN t_Organization t4 ON v1.fsupplyid=t4.FItemID
+Where (v1.FTranType = 21 AND (v1.FCancellation = 0)) and (t3.FNumber like '93.%' or t3.FHelpCode LIKE 'GQ%' )
+And Year(v1.FDate) in('2019','2020','2021','2022','2023')
+AND t4.f_131 LIKE '%富士通%'
+Group by Year(v1.FDate),t4.fname
+order by Year(v1.FDate),sum(u1.FConsignAmount) desc
