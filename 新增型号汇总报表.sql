8c4618e42e9b7bc1ccc5c4d373d2c3cb5e4e39f1
d0min0wang <38913318+d0min0wang@users.noreply.github.com>
2024-01-24 17:14:39 +0800

    Update 新增型号汇总报表.sql

--- 新增型号汇总报表.sql
+++ 新增型号汇总报表.sql
@@ -1,9 +1,23 @@
-select t3.fname,t1.fname,t1.FNumber,t1.f_102,t1.f_105,t2.FCreateDate,t2.fcreateuser from t_ICItem t1
+select t3.fname,t1.fname,t1.FNumber,t2.FCreateDate,t2.fcreateuser from t_ICItem t1
 LEFT JOIN t_BaseProperty t2 on	 t1.FItemID=t2.FItemID AND t2.FTypeID=3 
 LEFT JOIN t_Department t3 ON t1.FSource=t3.FItemID
-WHERE (YEAR(t2.FCreateDate) in ('2023') OR year(t1.f_105)='2023')
+WHERE YEAR(t2.FCreateDate) in ('2023') 
 AND t3.fname='电气连接事业部'
 
 
 select * FROM t_TableDescription WHERE ftablename='t_BaseProperty'
 SELECT * FROM t_FieldDescription WHERE FDescription LIKE '%创建%'
+
+
+SELECT t3.fname,count(DISTINCT t1.fsupplyid) FROM icstockbill t1
+left join t_organization t2 on t1.fsupplyid=t2.FItemID
+LEFT JOIN t_Department t3 ON t2.Fdepartment=t3.FItemID
+WHERE year(t1.FDate)='2021' 
+group BY t3.FName WITH ROLLUP
+
+SELECT t3.fname,count(DISTINCT t1.fsupplyid),sum(t4.FConsignAmount)/10000 FROM icstockbill t1
+left join t_organization t2 on t1.fsupplyid=t2.FItemID
+LEFT JOIN t_Department t3 ON t2.Fdepartment=t3.FItemID
+LEFT JOIN ICStockBillEntry t4 on t1.FInterID=t4.FInterID
+WHERE year(t1.FDate)='2021' 
+group BY t3.FName WITH ROLLUP
